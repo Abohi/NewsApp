7c8e2b655092712cda93366f3df4f61a835e78fb
Evans Abohi <abohievanson@gmail.com>
2022-07-16 13:40:28 +0100

    subview bug fixes

---
+++ NewsApp/ApiCaller.swift
@@ -0,0 +1,52 @@
+//
+//  ApiCaller.swift
+//  NewsApp
+//
+//  Created by Evans Abohi on 16/07/2022.
+//
+
+import Foundation
+final class APICaller{
+    static let shared=APICaller()
+    struct Constants{
+        static let topHeadLinesUrl=URL(string:"https://newsapi.org/v2/top-headlines?country=us&apiKey=d4a3cbe992474b258d51b4a7761679eb")
+    }
+    private init(){}
+    public func getTopStories(completion:@escaping(Result<[Article],Error>)->Void){
+        guard let url = Constants.topHeadLinesUrl else{
+            return
+        }
+        let task = URLSession.shared.dataTask(with: url) { data, response,error in
+            if let error = error{
+                completion(.failure(error))
+            }else if let data=data{
+                do{
+                    let result = try JSONDecoder().decode(APIResponse.self, from: data)
+                    print("Articles: \(result.articles.count)")
+                    completion(.success(result.articles))
+                }
+                catch{
+                    completion(.failure(error))
+                }
+            }
+        }
+        task.resume()
+        
+    }
+}
+//Models
+struct APIResponse:Codable{
+    let articles:[Article]
+}
+struct Article:Codable{
+    let source:Source
+    let title:String
+    let description:String
+    let url:String
+    let urlToImage:String
+    let publishedAt:String
+    
+}
+struct Source:Codable{
+    let name:String
+}
--- NewsApp/ViewController.swift
+++ NewsApp/ViewController.swift
@@ -4,16 +4,65 @@
 //
 //  Created by Evans Abohi on 16/07/2022.
 //
-
+//TableView
+// Custom Cell
+//Api Caller
+//Open the News Story
+//Search for news
+//d4a3cbe992474b258d51b4a7761679eb
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate,UITableViewDataSource{
+    
+    private let tableView: UITableView={
+        let table=UITableView()
+        table.register(NewsTableViewCell.self, forCellReuseIdentifier: NewsTableViewCell.indentifier)
+        return table;
+    }()
+    private var viewModels=[NewsTableViewCellViewModel]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title="News"
+        view.backgroundColor = .systemBackground
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        APICaller.shared.getTopStories { [weak self] result in
+            switch result{
+            case .success(let response):
+                self?.viewModels = response.compactMap({
+                    NewsTableViewCellViewModel(title: $0.title, subtitle:$0.description, imageURL: URL(string: $0.urlToImage))
+                })
+                DispatchQueue.main.async {
+                    self?.tableView.reloadData()
+                }
+                break
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
-
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame=view.bounds
+    }
+ 
+    //Table
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModels.count;
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+       guard let cell = tableView.dequeueReusableCell(withIdentifier: NewsTableViewCell.indentifier,for: indexPath) as? NewsTableViewCell else {
+            fatalError()
+        }
+        
+        cell.configure(with: viewModels[indexPath.row])
+        return cell;
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 
 }
 
